1fe726a7950687337e507f1b210c6460d68b3660
Freeman <freemanoparaocha@gmail.com>
2024-08-27 01:35:22 +0100

    added image link

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def fullscreen():
         location=[
             6.510235738797816, 3.3718822564828392
         ],
-        tooltip="Freeman is calling on you!",
-        popup="N.A is that you?",
+        tooltip="http://media-cache-ec0.pinimg.com/736x/19/52/4d/19524ddf1e4aa4c4b723ce603e606071.jpg",
+        popup="Testing the image link option in this pop-up",
         icon=folium.Icon(color="gray")
     ).add_to(m)
     return m.get_root().render()
