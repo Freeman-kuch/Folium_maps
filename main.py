6eee7b51353e15a7d4baefcc6a2c9eb16a4a0354
Freeman <freemanoparaocha@gmail.com>
2024-08-27 01:37:36 +0100

    added image link

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def fullscreen():
         location=[
             6.510235738797816, 3.3718822564828392
         ],
-        tooltip="http://media-cache-ec0.pinimg.com/736x/19/52/4d/19524ddf1e4aa4c4b723ce603e606071.jpg",
-        popup="Testing the image link option in this pop-up",
+        popup="http://media-cache-ec0.pinimg.com/736x/19/52/4d/19524ddf1e4aa4c4b723ce603e606071.jpg",
+        tooltip="Testing the image link option in this pop-up",
         icon=folium.Icon(color="gray")
     ).add_to(m)
     return m.get_root().render()
