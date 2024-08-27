4b86d032a8d3d424ddc9e1b0c6f57b2b1ff757e3
Freeman <freemanoparaocha@gmail.com>
2024-08-27 01:49:14 +0100

    added image link

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import folium
 
 app = Flask(__name__)
 
-
+image = folium.CustomIcon(
+"http://media-cache-ec0.pinimg.com/736x/19/52/4d/19524ddf1e4aa4c4b723ce603e606071.jpg",
+icon_size=(38, 95),
+)
 @app.route("/")
 def fullscreen():
     """Simple example of a fullscreen map."""
@@ -16,11 +19,10 @@ def fullscreen():
     )
     folium.Marker(
         location=[
-            6.510235738797816, 3.3718822564828392
+            6.510235738797400, 3.3718822564828392
         ],
-        popup="http://media-cache-ec0.pinimg.com/736x/19/52/4d/19524ddf1e4aa4c4b723ce603e606071.jpg",
-        tooltip="Testing the image link option in this pop-up",
-        icon=folium.Icon(color="gray")
+        icon=image,
+        popup="Testing the image link option in this pop-up",
     ).add_to(m)
     return m.get_root().render()
 
